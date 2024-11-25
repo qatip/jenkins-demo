99109e9b001be7d6bda3250af220ce709963abfa
qatip <michael2697@hotmail.com>
2024-11-25 13:30:17 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,14 +3,6 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "my-test-bucket-michael"
+  bucket = "my-example-bucket-mcg"
   acl    = "private"
-}
-
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket"
-    key            = "terraform/state/terraform.tfstate"
-    region         = "us-west-2"
-  }
 }
